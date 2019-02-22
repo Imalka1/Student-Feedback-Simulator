8f6626dcc9933e1c415872366d2e14e65a06c67c
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2019-02-22 14:42:05 +0530

    up3

--- out/artifacts/StudentFeedbackSimulator_war_exploded/marks_controller/marksController.js
+++ out/artifacts/StudentFeedbackSimulator_war_exploded/marks_controller/marksController.js
@@ -1,12 +1,12 @@
 var that;
 $('.tdMark').click(function () {
-    if ($(this).css("background-color") == "rgb(38, 104, 22)") {
+    if ($(this).css("background-color") == "rgb(48,118,29)") {
         $(this).css('background-color', "rgba(0, 0, 0, 0)");
         $(this).css('color', 'black');
     } else {
         $(this).parent().children('td.tdMark').css("background-color", "rgba(0, 0, 0, 0)");
         $(this).parent().children('td.tdMark').css("color", 'black');
-        $(this).css('background-color', "rgb(38,104,22)");
+        $(this).css('background-color', "rgb(48,118,29)");
         $(this).css('color', 'white');
     }
 });
